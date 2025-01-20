6ee53f1007a428e041f90f13afe6c6aa803b19e1
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2025-01-20 16:04:36 +0300

    add support for parity-scale-codec and scale-info

--- patch.sh
+++ patch.sh
@@ -34,3 +34,6 @@ git commit --amend --no-edit --date "$COMMIT_DATE"
 
 git am ../0006-chore-relax-validate_num_of_signers-restrictions.patch
 git commit --amend --no-edit --date "$COMMIT_DATE"
+
+git am ../0007-feat-add-support-for-parity-scale-codec-and-scale-info.patch
+git commit --amend --no-edit --date "$COMMIT_DATE"
